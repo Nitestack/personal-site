dcb5acc7d59ccd8256cd0dc82e1bbb6d0e2ce09c
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-03 17:53:45 +0100

    update: add shadcn ui for ui boilerplate

--- src/utils.ts
+++ src/utils.ts
@@ -9,6 +9,10 @@ export function classNames(...classLists: ClassValue[]) {
   return twMerge(clsx(classLists));
 }
 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function keysFromObject<T extends object>(obj: T) {
   return Object.keys(obj) as (keyof T)[];
 }
