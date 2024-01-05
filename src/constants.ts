a7d56d298ff342f50ed3ca8e38212a2b8703258f
Nitestack <code@npham.de>
2024-01-05 22:54:12 +0100

    feat: create DEFAULT_LOCALE constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const LOCALES = ["en", "de"];
 
+export const DEFAULT_LOCALE = "en";
+
 export const LOCALE_PREFIX = "always";
 
 export const LOCALE_MAP: Record<string, string> = {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,22 @@
-import { LOCALES, LOCALE_PREFIX } from "@constants";
 import createMiddleware from "next-intl/middleware";
+
+import { DEFAULT_LOCALE, LOCALES, LOCALE_PREFIX } from "@constants";
 
 export default createMiddleware({
   locales: LOCALES,
-  defaultLocale: "en",
+  defaultLocale: DEFAULT_LOCALE,
   localePrefix: LOCALE_PREFIX,
 });
 
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)", "/(de|en)/:path*"],
+  matcher: [
+    // Enable a redirect to a matching locale at the root
+    "/",
+    // Set a cookie to remember the previous locale for
+    // all requests that have a locale prefix
+    "/(de|en)/:path*",
+    // Enable redirects that add missing locales
+    // (e.g. `/pathnames` -> `/en/pathnames`)
+    "/((?!api|_next|_vercel|.*\\..*).*)",
+  ],
 };
