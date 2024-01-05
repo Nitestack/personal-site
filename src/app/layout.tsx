cc0dd28eb779ab83267e44ba0a754d79e9f42751
Nitestack <code@npham.de>
2024-01-05 23:10:47 +0100

    fix: move metadata generation to the locale layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,6 @@
 import "@app/globals.css";
 
-import { type Metadata } from "next";
 import { type FC, type ReactNode } from "react";
-
-export const metadata: Metadata = {
-  title: {
-    default: "Nhan Pham",
-    template: "%s | Nhan Pham",
-  },
-  description: "Personal website",
-};
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return children;
