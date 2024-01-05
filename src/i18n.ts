c20991d06f06a547f1633c121f256434015d05e4
Nitestack <code@npham.de>
2024-01-05 22:59:44 +0100

    style: fix linting errors

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,13 @@
-import { LOCALES } from "@constants";
 import { getRequestConfig } from "next-intl/server";
 import { notFound } from "next/navigation";
+
+import { LOCALES } from "@constants";
 
 export default getRequestConfig(async ({ locale }) => {
   if (!LOCALES.includes(locale)) notFound();
 
   return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
