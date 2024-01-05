60e30da7574fe58e1735e567fd8d10e9bb534279
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-05 22:51:22 +0100

    refactor: add description

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,10 @@
 import Error from "next/error";
 import { type FC } from "react";
 
+// Render the default Next.js 404 page when a route
+// is requested that doesn't match the middleware and
+// therefore doesn't have a locale associated with it.
+
 const NotFound: FC = () => {
   return (
     <html lang="en">
