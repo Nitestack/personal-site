3b4ec749950ff4b6e11fc9d6bddf7adfcc7b27eb
Nitestack <code@npham.de>
2024-01-07 00:43:24 +0100

    feat: add placeholders for different sections

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,8 @@
+import AboutSection from "@components/sections/about";
+import ContactSection from "@components/sections/contact";
+import ExperienceSection from "@components/sections/experience";
+import ProjectsSection from "@components/sections/projects";
+
 import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 import { type FC } from "react";
@@ -7,9 +12,16 @@ const HomePage: FC<{ params: { locale: string } }> = ({
 }) => {
   unstable_setRequestLocale(locale);
 
-  const t = useTranslations("Settings");
+  const t = useTranslations("Routes");
 
-  return <div>{t("language")}</div>;
+  return (
+    <div className="space-y-6">
+      <AboutSection heading={t("about")} />
+      <ExperienceSection heading={t("experience")} />
+      <ProjectsSection heading={t("projects")} />
+      <ContactSection heading={t("contact")} />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/components/sections/projects.tsx
@@ -0,0 +1,26 @@
+import { type FC } from "react";
+
+const ProjectsSection: FC<{ heading: string }> = ({ heading }) => {
+  return (
+    <section>
+      <h1 className="text-3xl mb-3" id="projects">
+        {heading}
+      </h1>
+      <p className="tracking-wide italic">
+        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+        Lorem duis laboris cupidatat officia voluptate. Culpa proident
+        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
+        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
+        et culpa duis.
+      </p>
+    </section>
+  );
+};
+
+export default ProjectsSection;
