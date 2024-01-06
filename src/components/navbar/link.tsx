ba288e7727932051a58a13916c7b88853eee5571
Nitestack <code@npham.de>
2024-01-07 00:51:41 +0100

    refactor: fix type errors

--- src/components/navbar/link.tsx
+++ src/components/navbar/link.tsx
@@ -3,19 +3,19 @@
 import { navigationMenuTriggerStyle } from "@components/ui/navigation-menu";
 
 import { Link, usePathname } from "@navigation";
+
 import {
   NavigationMenuLink,
   type NavigationMenuLinkProps,
 } from "@radix-ui/react-navigation-menu";
+import { type UrlObject } from "node:url";
 import { type FC } from "react";
 
 import { classNames } from "@utils";
 
-const NavbarLink: FC<NavigationMenuLinkProps & { href: string }> = ({
-  href,
-  className,
-  ...props
-}) => {
+const NavbarLink: FC<
+  Omit<NavigationMenuLinkProps, "href"> & { href: string | UrlObject }
+> = ({ href, className, ...props }) => {
   const pathname = usePathname();
   return (
     <Link href={href} passHref legacyBehavior>
