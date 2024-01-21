37674514ad4189c2ef936ac5af71cebf4c6dc8d5
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-21 19:20:08 +0100

    feat: use Github Dark Dimmed theme for syntax highlighting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@app/globals.css";
 
+import "highlight.js/styles/github-dark-dimmed.css";
 import { type FC, type ReactNode } from "react";
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
