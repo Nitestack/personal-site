3c154ed7a1ef0dfe7ab9b04b5a3488bf9d3a1237
Nitestack <code@npham.de>
2024-01-24 00:17:31 +0100

    feat: improve blog responsiveness

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -100,15 +100,17 @@ const BlogPost: FC<{
   const createdDate = getLocaleDateString(new Date(post.created_time));
 
   return (
-    <article className="max-w-3xl mx-auto">
-      <section className="space-y-8 py-12">
-        <div className="text-center">
-          <h1 className="text-balance tracking-wide text-3xl font-extrabold md:text-5xl">
+    <article className="max-w-3xl mx-auto mt-4 md:mt-8 lg:mt-12 space-y-4 md:space-y-8">
+      <section className="space-y-4 md:space-y-8">
+        <div className="text-center space-y-4 md:space-y-8 lg:space-y-12">
+          <h1 className="text-balance tracking-wide text-3xl sm:text-4xl font-extrabold lg:text-5xl">
             {title}
           </h1>
-          <p className="mt-4 lg:text-lg font-mono">{excerpt}</p>
+          <p className="mt-4 text-balance text-sm sm:text-base lg:text-lg font-mono md:italic">
+            {excerpt}
+          </p>
         </div>
-        <div className="flex items-center justify-between gap-4">
+        <div className="flex flex-row items-center justify-between gap-4">
           <div className="flex items-center gap-2">
             <Avatar>
               <AvatarImage src={Logo.src} alt={SITE_CONFIG.name} />
@@ -117,19 +119,21 @@ const BlogPost: FC<{
               </AvatarFallback>
             </Avatar>
             <div>
-              <p className="text-xs font-mono">{authorLabel}</p>
-              <p>{SITE_CONFIG.name}</p>
+              <p className="text-xs md:text-sm font-mono">{authorLabel}</p>
+              <p className="text-sm md:text-lg font-bold">{SITE_CONFIG.name}</p>
             </div>
           </div>
           <div className="text-right flex items-start gap-10">
             <div>
-              <p className="text-xs font-mono">{createdAtLabel}</p>
-              <p>{createdDate}</p>
+              <p className="text-xs md:text-sm font-mono">{createdAtLabel}</p>
+              <p className="text-sm md:text-lg font-bold">{createdDate}</p>
             </div>
             {createdDate.toLowerCase() !== lastEditedDate.toLowerCase() && (
-              <div>
-                <p className="text-xs font-mono">{lastEditedLabel}</p>
-                <p>{lastEditedDate}</p>
+              <div className="hidden sm:block">
+                <p className="text-xs md:text-sm font-mono">
+                  {lastEditedLabel}
+                </p>
+                <p className="text-sm md:text-lg font-bold">{lastEditedDate}</p>
               </div>
             )}
           </div>
