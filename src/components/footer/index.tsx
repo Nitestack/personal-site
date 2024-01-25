cd58f677b7110577c4a2d9441d5a1ed6072a16b0
Nitestack <code@npham.de>
2024-01-25 23:25:50 +0100

    fix: show footer in light mode

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -9,7 +9,7 @@ const Footer: FC = () => {
   const t = useTranslations("Footer");
   return (
     <footer className="border-t">
-      <div className="container mx-auto px-6 py-4 text-center text-white">
+      <div className="container mx-auto px-6 py-4 text-center">
         <h3 className="text-xl font-bold">Let's Connect</h3>
         <p className="mt-2">Email: {SITE_CONFIG.email}</p>
         <div className="flex justify-center mt-4 space-x-2">
