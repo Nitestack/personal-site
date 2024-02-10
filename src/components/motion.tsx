ae4c31f508299f1d3dd9915f1817bdd3fa59873b
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-10 13:27:57 +0100

    feat: add motion section

--- src/components/motion.tsx
+++ src/components/motion.tsx
@@ -6,3 +6,4 @@ export const MotionDiv = motion.div;
 export const MotionA = motion.a;
 export const MotionUl = motion.ul;
 export const MotionFooter = motion.footer;
+export const MotionSection = motion.section;
