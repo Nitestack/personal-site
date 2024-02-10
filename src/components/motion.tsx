206d231df37251d3274a169f576bf8964e44f0c1
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-10 12:54:37 +0100

    feat: add basic motion components

---
+++ src/components/motion.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const MotionDiv = motion.div;
+export const MotionA = motion.a;
+export const MotionUl = motion.ul;
+export const MotionFooter = motion.footer;
