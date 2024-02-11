9d5c98e63c4932860373ab581a29b7a9730a1533
Nitestack <code@npham.de>
2024-02-11 13:19:19 +0100

    feat: put config into centralized constants

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -1,39 +1,30 @@
 import Section from "@components/sections/section";
-import ExperienceTimeline, {
-  type ExperienceTimelineItem,
-} from "@components/ui/timeline";
+import ExperienceTimeline from "@components/ui/timeline";
 
 import { useTranslations } from "next-intl";
 import { type FC } from "react";
 
 import { classNames } from "@utils";
 
+import { SITE_CONFIG } from "@constants";
+
 const ExperienceSection: FC<{ locale: string }> = ({ locale }) => {
   const t = useTranslations();
-
-  const events: ExperienceTimelineItem[] = [
-    {
-      date: "6-2022",
-      duration: [2, "week"],
-      title: t("Experience.schoolInternship"),
-      company: "von Borstel GmbH",
-      companyLink: "https://www.von-borstel.de",
-      description: t("Experience.CompanyDescriptions.vonBorstel"),
-      latest: true,
-    },
-    {
-      date: "6-2021",
-      duration: [2, "week"],
-      title: t("Experience.schoolInternship"),
-      company: "MBJ Solutions GmbH",
-      companyLink: "https://www.mbj-solutions.com",
-      description: t("Experience.CompanyDescriptions.mbjSolutions"),
-    },
-  ];
   return (
     <Section heading={t("Routes.experience")} sectionID="experience">
       <div className="space-y-8">
-        <ExperienceTimeline locale={locale} items={events} />
+        <ExperienceTimeline
+          locale={locale}
+          items={SITE_CONFIG.experience.events.map(
+            ({ title, description, ...restEvent }) => ({
+              ...restEvent,
+              // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+              title: t(`Experience.JobTitles.${title}`),
+              // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+              description: t(`Experience.CompanyDescriptions.${description}`),
+            }),
+          )}
+        />
       </div>
     </Section>
   );
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { type ExperienceTimelineItem } from "@components/ui/timeline";
+
 import { type UrlObject } from "node:url";
 import { type LocalePrefix } from "node_modules/next-intl/dist/types/src/shared/types";
 
@@ -5,6 +7,15 @@ interface NavigationRoute {
   id: Section;
   translationKey: keyof Messages["Routes"];
   url: string | UrlObject;
+}
+
+interface Skill {
+  name: string;
+  level: "Beginner" | "Intermediate" | "Advanced" | "Expert";
+  info?: string;
+  imageUrl: string;
+  bgColor?: string;
+  textColor?: string;
 }
 
 export const sections = [
@@ -25,11 +36,60 @@ export const SITE_CONFIG: {
   birthday: Date;
   url: string;
   routes: NavigationRoute[];
+  experience: {
+    events: (Omit<ExperienceTimelineItem, "title" | "description"> & {
+      title: keyof Messages["Experience"]["JobTitles"];
+      description: keyof Messages["Experience"]["CompanyDescriptions"];
+    })[];
+    skills: Skill[];
+  };
 } = {
   firstName: "Nhan",
   name: "Nhan Pham",
   email: "nhan.pham@mail.de",
   birthday: new Date(2006, 2, 6),
+  experience: {
+    events: [
+      {
+        date: "6-2022",
+        duration: [2, "week"],
+        title: "schoolInternship",
+        company: "von Borstel GmbH",
+        companyLink: "https://www.von-borstel.de",
+        description: "mbjSolutions",
+        latest: true,
+      },
+      {
+        date: "6-2021",
+        duration: [2, "week"],
+        title: "schoolInternship",
+        company: "MBJ Solutions GmbH",
+        companyLink: "https://www.mbj-solutions.com",
+        description: "mbjSolutions",
+      },
+    ],
+    skills: [
+      {
+        name: "Javascript",
+        level: "Intermediate",
+        imageUrl: "/images/logos/javascript.svg",
+        bgColor: "bg-[#F7DF1E]",
+        textColor: "text-[#323330]",
+      },
+      {
+        name: "TypeScript",
+        level: "Intermediate",
+        imageUrl: "/images/logos/typescript.svg",
+        bgColor: "bg-[#3178C6]",
+        textColor: "text-[#FAF9F8]",
+      },
+      {
+        name: "Next.js",
+        level: "Intermediate",
+        imageUrl: "/images/logos/nextjs.svg",
+      },
+    ],
+  },
   url:
     process.env.NODE_ENV == "development"
       ? "http://localhost:3000"
