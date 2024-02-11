1d53698763cdb54b24f9b4cfc66563095097eec5
Nitestack <code@npham.de>
2024-02-11 12:58:51 +0100

    feat: faster animation on about paragraphs

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -43,7 +43,7 @@ const AboutSection: FC = () => {
         <MotionDiv
           initial={{ opacity: 0, x: "100%" }}
           whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 0.5 }}
           className="flex-1 text-balance space-y-5"
         >
           <p>
