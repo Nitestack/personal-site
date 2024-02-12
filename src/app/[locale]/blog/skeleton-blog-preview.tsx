b07c09ff3ed561dca44b7f3442e79a9db0d4f2eb
Nitestack <code@npham.de>
2024-02-12 13:15:57 +0100

    feat: update skeleton of blog preview

--- src/app/[locale]/blog/skeleton-blog-preview.tsx
+++ src/app/[locale]/blog/skeleton-blog-preview.tsx
@@ -5,30 +5,21 @@ import { type FC } from "react";
 
 const SkeletonBlogPreview: FC = () => {
   return (
-    <Card className="overflow-hidden flex flex-col">
-      <Skeleton className="w-full aspect-video rounded-none h-full" />
+    <Card className="overflow-hidden min-w-72 lg:min-w-96 max-w-md flex flex-col snap-center">
+      <Skeleton className="h-full aspect-video rounded-none" />
       <CardHeader className="flex flex-col h-full space-y-6">
         <div className="flex-1 space-y-4">
           <div className="space-y-2 flex flex-col items-center">
             <Skeleton className="h-5 w-11/12" />
-            <Skeleton className="h-5 w-8/12" />
+            <Skeleton className="hidden md:block h-5 w-8/12" />
           </div>
           <div className="space-y-2">
-            <Skeleton className="h-4 w-11/12" />
-            <Skeleton className="h-4 w-3/4" />
-            <Skeleton className="h-4 w-5/6" />
+            <Skeleton className="h-3 sm:h-4 w-11/12" />
+            <Skeleton className="h-3 sm:h-4 w-3/4" />
+            <Skeleton className="h-3 sm:h-4 w-5/6" />
           </div>
         </div>
-        <div className="flex justify-between items-end">
-          <div className="space-y-2">
-            <Skeleton className="h-3 w-20" />
-            <Skeleton className="h-4 w-36" />
-          </div>
-          <div className="space-y-2 flex items-end flex-col">
-            <Skeleton className="h-3 w-20" />
-            <Skeleton className="h-4 w-36" />
-          </div>
-        </div>
+        <Skeleton className="p-2 w-full h-full" />
       </CardHeader>
     </Card>
   );
