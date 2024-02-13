ddab4a95a5edbf48501d3eca9f6c495425961019
Nitestack <code@npham.de>
2024-02-13 01:06:06 +0100

    feat: add basic contact section

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
+import LucideIcon from "@components/lucide-icon";
 import { MotionFooter } from "@components/motion";
 
-import { Github, MessageCircle, Twitter } from "lucide-react";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { type FC } from "react";
@@ -17,7 +17,7 @@ const Footer: FC = () => {
       transition={{ duration: 1.5 }}
     >
       <div className="container mx-auto px-6 py-4 text-center">
-        <h3 className="text-xl font-bold">Let's Connect</h3>
+        <h3 className="text-xl font-bold">{new URL(SITE_CONFIG.url).host}</h3>
         <Link
           className="mt-2 text-primary hover:underline"
           href={`mailto:${SITE_CONFIG.email}`}
@@ -25,15 +25,11 @@ const Footer: FC = () => {
           Email: {SITE_CONFIG.email}
         </Link>
         <div className="flex justify-center mt-4 space-x-2">
-          <Link target="_blank" href="/twitter">
-            <Twitter className="h-6 w-6" />
-          </Link>
-          <Link target="_blank" href="/github">
-            <Github className="h-6 w-6" />
-          </Link>
-          <Link target="_blank" href="/discord">
-            <MessageCircle className="h-6 w-6" />
-          </Link>
+          {SITE_CONFIG.socials.map(({ name, url, iconName }) => (
+            <Link key={name} target="_blank" href={url}>
+              <LucideIcon name={iconName} className="w-6 h-6" />
+            </Link>
+          ))}
         </div>
         <p className="mt-4 text-sm">
           © {new Date().getFullYear()} {SITE_CONFIG.name}.{" "}
--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,15 +1,48 @@
+import LucideIcon from "@components/lucide-icon";
 import Section from "@components/sections/section";
+import { Button } from "@components/ui/button";
 
+import { Mail, MapPin } from "lucide-react";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 import { type FC } from "react";
+
+import { SITE_CONFIG } from "@constants";
 
 const ContactSection: FC = () => {
   const t = useTranslations();
   return (
-    <Section heading={t("Routes.contact")} sectionID="contact">
-      <p className="tracking-wider uppercase text-2xl text-center">
-        {t("All.comingSoon")}
-      </p>
+    <Section
+      className="min-h-fit"
+      heading={t("Routes.contact")}
+      sectionID="contact"
+    >
+      <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
+        <div className="mx-auto max-w-sm grid gap-1.5 text-left rounded-lg border border-border p-4">
+          <p className="flex items-center gap-2">
+            <Mail className="w-5 h-5" />
+            <Link
+              className="hover:underline font-medium"
+              href={`mailto:${SITE_CONFIG.email}`}
+            >
+              {SITE_CONFIG.email}
+            </Link>
+          </p>
+          <p className="flex items-center gap-2">
+            <MapPin className="w-5 h-5" />
+            <span className="font-medium">{SITE_CONFIG.location}</span>
+          </p>
+        </div>
+        <div className="flex justify-center space-x-4 p-2 rounded-lg border border-border">
+          {SITE_CONFIG.socials.map(({ name, url, iconName }) => (
+            <Button size="icon" variant="ghost" asChild>
+              <Link key={name} target="_blank" href={url}>
+                <LucideIcon name={iconName} className="w-8 h-8" />
+              </Link>
+            </Button>
+          ))}
+        </div>
+      </div>
     </Section>
   );
 };
