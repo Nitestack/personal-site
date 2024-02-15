adc73f73143fcac180c231df86ab72962089adba
Nitestack <code@npham.de>
2024-02-15 19:33:28 +0100

    style: disable linting errors

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 type Messages = typeof import("./messages/en.json");
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 declare interface IntlMessages extends Messages {}
