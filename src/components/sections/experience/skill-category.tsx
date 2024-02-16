477fe2b199bd15409b7b314176aef39af03cd6e8
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-16 19:45:28 +0100

    feat: add accordions to toggle skill categories

--- src/components/sections/experience/skill-category.tsx
+++ src/components/sections/experience/skill-category.tsx
@@ -1,40 +1,43 @@
 "use client";
 
-import { AnimatePresence, MotionDiv } from "@components/motion";
+import { AnimatePresence } from "@components/motion";
 import SkillCard from "@components/sections/experience/skill-card";
 import { useSkillContext } from "@components/sections/experience/skill-context";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@components/ui/accordion";
 
 import { type FC } from "react";
 
 import { type Skill } from "@constants";
 
 const SkillCategory: FC<{
+  id: string;
   name: string;
   skills: Skill[];
-}> = ({ name, skills }) => {
+}> = ({ id, name, skills }) => {
   const { isValid } = useSkillContext();
   return (
-    <MotionDiv
-      className="flex items-center justify-center flex-col gap-4"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{
-        delay: 0.25,
-      }}
-    >
-      <h3 className="text-xl tracking-widest uppercase">{name}</h3>
-      <div className="flex md:justify-center overflow-x-auto md:overflow-x-hidden w-full rounded-xl p-1 snap-x snap-mandatory">
-        <div className="flex flex-nowrap md:flex-wrap justify-center gap-2 md:gap-4 lg:gap-6">
-          <AnimatePresence>
-            {skills
-              .filter((skill) => isValid(skill.level))
-              .map((skill) => (
-                <SkillCard key={skill.name} {...skill} />
-              ))}
-          </AnimatePresence>
+    <AccordionItem value={id} className="border-0">
+      <AccordionTrigger className="justify-center gap-2">
+        <h3 className="text-xl tracking-widest uppercase">{name}</h3>
+      </AccordionTrigger>
+      <AccordionContent>
+        <div className="flex md:justify-center overflow-x-auto md:overflow-x-hidden w-full rounded-xl p-1 snap-x snap-mandatory">
+          <div className="flex flex-nowrap md:flex-wrap justify-center gap-2 md:gap-4 lg:gap-6">
+            <AnimatePresence>
+              {skills
+                .filter((skill) => isValid(skill.level))
+                .map((skill) => (
+                  <SkillCard key={`${id}-${skill.name}`} {...skill} />
+                ))}
+            </AnimatePresence>
+          </div>
         </div>
-      </div>
-    </MotionDiv>
+      </AccordionContent>
+    </AccordionItem>
   );
 };
 
