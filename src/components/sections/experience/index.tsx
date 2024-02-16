7842673c844fdee9d6172b4a9fa663f69be60d8e
Nitestack <code@npham.de>
2024-02-16 19:15:15 +0100

    feat: add tech stack for experience section

--- src/components/sections/experience/index.tsx
+++ src/components/sections/experience/index.tsx
@@ -19,7 +19,7 @@ const ExperienceSection: FC<{ locale: string }> = ({ locale }) => {
             <ExperienceTimeline
               locale={locale}
               items={SITE_CONFIG.experience.events.map(
-                ({ title, description, ...restEvent }) => ({
+                ({ title, description, techStack, ...restEvent }) => ({
                   ...restEvent,
                   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
                   title: t(`Experience.JobTitles.${title}`),
@@ -27,6 +27,7 @@ const ExperienceSection: FC<{ locale: string }> = ({ locale }) => {
                   description: t(
                     `Experience.CompanyDescriptions.${description}`,
                   ),
+                  tags: techStack,
                 }),
               )}
             />
