0be74a3e13d335a20e7625a3a1e834809154f534
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-18 18:06:37 +0100

    feat: add project filter

---
+++ src/components/sections/projects/list.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { AnimatePresence } from "@components/motion";
+import Project, {
+  type TranslatedProject,
+} from "@components/sections/projects/project";
+import { useProjectStatusContext } from "@components/sections/projects/status-context";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@components/ui/carousel";
+
+import { type FC } from "react";
+
+const ProjectList: FC<{ projects: TranslatedProject[] }> = ({ projects }) => {
+  const { isValid } = useProjectStatusContext();
+  return (
+    <div className="px-7 lg:max-w-5xl mx-auto">
+      <Carousel className="w-full max-w-full" opts={{ loop: true }}>
+        <CarouselContent>
+          <AnimatePresence>
+            {projects
+              .filter((project) => isValid(project.status))
+              .map((project) => (
+                <CarouselItem key={project.name} className="md:basis-1/2">
+                  <Project key={project.name} {...project} />
+                </CarouselItem>
+              ))}
+          </AnimatePresence>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default ProjectList;
---
+++ src/components/sections/projects/status-context.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import {
+  type FC,
+  type ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+
+import { type ProjectStatus, status } from "@constants";
+
+interface ProjectStatusType {
+  filteredProjectStatuses: ProjectStatus[];
+  toggleProjectStatus: (status: ProjectStatus) => () => void;
+  isValid: (status: ProjectStatus) => boolean;
+}
+
+const ProjectStatusContext = createContext<ProjectStatusType | null>(null);
+
+export const ProjectStatusContextProvider: FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [projectStatuses, setProjectStatuses] = useState<ProjectStatus[]>(
+    status as unknown as ProjectStatus[],
+  );
+
+  const toggleProjectStatus = useCallback(
+    (status: ProjectStatus) => {
+      return () => {
+        if (projectStatuses.includes(status)) {
+          setProjectStatuses((currentProjectStatuses) =>
+            currentProjectStatuses.filter((s) => s !== status),
+          );
+        } else {
+          setProjectStatuses((currentProjectStatuses) => [
+            ...currentProjectStatuses,
+            status,
+          ]);
+        }
+      };
+    },
+    [projectStatuses],
+  );
+  const isValid = useCallback(
+    (status: ProjectStatus) => projectStatuses.includes(status),
+    [projectStatuses],
+  );
+
+  return (
+    <ProjectStatusContext.Provider
+      value={{
+        isValid,
+        toggleProjectStatus,
+        filteredProjectStatuses: projectStatuses,
+      }}
+    >
+      {children}
+    </ProjectStatusContext.Provider>
+  );
+};
+
+export function useProjectStatusContext() {
+  const context = useContext(ProjectStatusContext);
+
+  if (context === null) {
+    throw new Error(
+      "useProjectStatusContext must be inside of <ProjectStatusContextProvider />",
+    );
+  }
+
+  return context;
+}
