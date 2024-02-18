0fe4f5eca8a9aae225e506562fd19bd79f9763bb
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-18 03:02:51 +0100

    feat: add project status

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,3 +1,4 @@
+import { MotionDiv } from "@components/motion";
 import Section from "@components/sections/section";
 import {
   Card,
@@ -22,18 +23,45 @@ import { type FC } from "react";
 
 import { getGitHubRepositoryUrl } from "@utils";
 
-import { SITE_CONFIG } from "@constants";
+import { type ProjectStatus, SITE_CONFIG, status } from "@constants";
+
+function getProjectStatusIcon(status: ProjectStatus) {
+  return status == "active"
+    ? "🟢"
+    : status == "completed"
+      ? "✅"
+      : status == "developing"
+        ? "🛠️"
+        : "📥";
+}
 
 const ProjectsSection: FC = () => {
   const t = useTranslations();
   return (
     <Section
-      className="text-center"
+      className="text-center space-y-4 md:space-y-6 lg:space-y-8"
       heading={t("Routes.projects")}
       sectionID="projects"
     >
+      <div className="flex items-center justify-center">
+        <div className="flex flex-col gap-1 p-4 overflow-x-hidden rounded-lg border border-border">
+          {status.map((status) => (
+            <MotionDiv
+              className="flex items-center gap-3"
+              initial={{ opacity: 0, x: "100%" }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <div className="flex items-center gap-0.5">
+                {getProjectStatusIcon(status)}
+              </div>
+              <p>{t(`Projects.Status.${status}`)}</p>
+            </MotionDiv>
+          ))}
+        </div>
+      </div>
       <div className="px-7 lg:max-w-5xl mx-auto">
-        <Carousel className="w-full" opts={{ loop: true }}>
+        <Carousel className="w-full max-w-full" opts={{ loop: true }}>
           <CarouselContent>
             {SITE_CONFIG.projects.map((project) => (
               <CarouselItem key={project.name} className="md:basis-1/2">
@@ -58,9 +86,14 @@ const ProjectsSection: FC = () => {
                       />
                     </div>
                     <CardHeader className="py-2 md:py-4 flex-1 space-y-2 group-hover:bg-accent">
-                      <CardTitle className="text-center text-balance font-bold">
-                        {project.name}
-                      </CardTitle>
+                      <div className="flex items-center justify-center gap-2">
+                        <CardTitle className="font-bold text-balance break-all">
+                          {project.name}{" "}
+                        </CardTitle>
+                        <span className="text-base p-0.5 bg-muted rounded">
+                          {getProjectStatusIcon(project.status)}
+                        </span>
+                      </div>
                       <CardDescription className="md:text-base text-balance">
                         {/* eslint-disable-next-line @typescript-eslint/restrict-template-expressions */}
                         {t(`Projects.Descriptions.${project.description}`)}
