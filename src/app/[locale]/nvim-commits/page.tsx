4d5f736870943adf4a9f298b5f2245558560471e
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-12 13:53:18 +0200

    fix: fix build error

--- src/app/[locale]/nvim-commits/page.tsx
+++ src/app/[locale]/nvim-commits/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain  */
+
 import { metadata } from "@metadata";
 import { getLocaleDateString } from "@utils";
 import { intlFormatDistance } from "date-fns";
